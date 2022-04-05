ebfd1802644d2fe724a8855e8c52d93e60353ecd
aldulescano <lescanoa@udesa.edu.ar>
2022-04-05 20:23:45 -0300

    arreglando errores

--- database/users.js
+++
@@ -1,7 +0,0 @@
-const users = [
-    {"id": "1", "username": "lescanoa@udesa.edu.ar", "password": "computadora1", "birthdate": 10-04-2000, "DNI":11223344 , "profilePhoto":  "https://media-exp1.licdn.com/dms/image/C4D03AQFzhMAdPuaasA/profile-displayphoto-shrink_100_100/0/1645141922088?e=1652313600&v=beta&t=ctxI0vWXhwOOBFeAZ6vB7ZSz8wxqpkeRve8s-ZVbjpk"},
-    {"id": "2", "username": "micaelad@udesa.edu.ar", "password": "mouse45", "birthdate": 15-06-2002, "DNI":12983344 , "profilePhoto":  "https://media-exp1.licdn.com/dms/image/C4E03AQGnIGS6zKL4eQ/profile-displayphoto-shrink_200_200/0/1611806646644?e=1652918400&v=beta&t=Q63Tdwtkwe59sbpZwSfXwWNRQzsDSCj2cw0akFGCRrM"},
-    {"id": "3", "username": "kims@udesa.edu.ar", "password": "computadora2", "birthdate": 05-08-2012, "DNI":22334455 , "profilePhoto":  "https://i.pinimg.com/280x280_RS/a2/a9/9d/a2a99d9f00dc311543850081aff3c706.jpg"}
-    
-   
-  ];
