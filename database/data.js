5e48ac0cda597c27bae8cd3a65506fb8759a36fb
MartinaArnaudeS <marnaude@udesa.edu.ar>
2022-04-12 11:49:44 -0300

    mini cambios en base de datos, agregue fotos y saque errores de ortografia

--- database/data.js
+++ database/data.js
@@ -3,16 +3,16 @@ const user = {
 }
 
 const products = [
-    {name: "Cepillo", description: "Un cepillo de bamboo.", pictureProduct:""},
+    {name: "Cepillo", description: "Un cepillo de bamboo.", pictureProduct:'/images/products/Cepillo.png'},
     {name: "Aceite de Almendras", description: "Aceite con propiedades rejuvenecedoras.", pictureProduct:'/images/products/AceiteDeAlmendrasOrganico.png'},
-    {name: "Balsamo Labial", description: "Hidrate sus labios cuiando al medio ambiente.", pictureProduct:""},
-    {name: "Esponja Vegetal", description: "Tiene una vida util larga sin involucrar procesos que contaminen nuestra querida tierra.", pictureProduct:""},
-    {name: "Jabón Solido", description: "Jabón no liquido. Formulado con aceites vegetales puras.", pictureProduct:""},
-    {name: "Pata de dientes", description: "Regula el PH de la boca. No contine fluor, ni ingredientes tóxicos. Cuida a los pinguinos, mientras cuidas tu dientes.", pictureProduct:""},
-    {name: "Peine de madera", description: "Piene de madera elaborado con madera de paraíso. No generan frizz.", pictureProduct:""},
-    {name: "Repelente de insectos", description: "Off vegano. Reune los aramos de varias de las plantas más utilizadas para repeler insectos.", pictureProduct:""},
-    {name: "Serúm Facial", description: "Combina los aceites más aclamados en el mundo de la cosmetica natural. Tiene propiedades nutrtivas y regeneradoras.", pictureProduct:""},
-    {name: "Shampoo Solido", description: "Shapoo no liquido. Forma más amigable de lavar tu cabello. Formulada con ingredientes que limpian suavemente y aportan brillo.", pictureProduct:""}
+    {name: "Bálsamo Labial", description: "Hidrate sus labios cuidando al medio ambiente.", pictureProduct:'/images/products/BalsamoLabial.png'},
+    {name: "Esponja Vegetal", description: "Tiene una vida útil larga sin involucrar procesos que contaminen nuestra querida tierra.", pictureProduct:'/images/products/EsponjaVegetal.png'},
+    {name: "Jabón Sólido", description: "Jabón no líquido. Formulado con aceites vegetales puras.", pictureProduct:'/images/products/JabonSolido.png'},
+    {name: "Pasta de dientes", description: "Regula el PH de la boca. No contiene flúor, ni ingredientes tóxicos. Cuida a los pingüinos, mientras cuidas tu dientes.", pictureProduct:'/images/products/PastaDental.png'},
+    {name: "Peine de madera", description: "Piene de madera elaborado con madera de paraíso. No generan frizz.", pictureProduct:'/images/products/PeineMadera.png'},
+    {name: "Repelente de insectos", description: "Off vegano. Reúne los aromas de varias de las plantas más utilizadas para repeler insectos.", pictureProduct:'/images/products/RepelenteDeInsectos.png'},
+    {name: "Serum Facial", description: "Combina los aceites más aclamados en el mundo de la cosmética natural. Tiene propiedades nutritivas y regeneradoras.", pictureProduct:'/images/products/SerumFacialRegenerador.png'},
+    {name: "Shampoo Solido", description: "Shampoo no líquido. Forma más amigable de lavar tu cabello. Formulada con ingredientes que limpian suavemente y aporta brillo.", pictureProduct:'/images/products/ShampooSolido.png'}
 ];
 
 const comments = [];
