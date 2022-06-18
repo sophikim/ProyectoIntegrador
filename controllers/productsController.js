eb897f2de3c5da7484cedb9dd0b01436e19d5054
MartinaArnaudeS <marnaude@udesa.edu.ar>
2022-06-18 11:20:06 -0300

    pagina de edit, controlador de edit, rutas de edit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,71 +5,108 @@ let product = db.Product;
 
 
 const productsController = {
-    index: function(req, res) {
-     product.findAll({
-       include: [
-         {association: "owner"},
-         {association: "comments"}
-       ]
-     }).then(function(Product){
+  index: function (req, res) {
+    product.findAll({
+        include: [{
+            association: "owner"
+          },
+          {
+            association: "comments"
+          }
+        ]
+      }).then(function (Product) {
         res.render('product', {
           products: Product,
           comments: Comments,
           user: db.User,
         });
       })
-      .catch(function(error){
+      .catch(function (error) {
         res.send(error)
       });
-    },
-    show: function(req, res) {
-      product.findByPk(req.params.id, {
-        include: {all: true, nested:true}
-      }).then(function(Product){
-          console.log(Product.dataValues);
-         res.render('product', {
-           products: Product,
-           comments: Comments,
-           user: db.User,
-         })
-       })
-       .catch(function(error){
-         res.send(error)
-       });
-     },
-    productAdd: function(req, res) {
-      if(!req.session.user){
-      throw Error ('Not authorized') 
-      }
-     res.render('product-add') 
-    }, 
-    storeAdd: function(req, res) {
-      if (!req.session.user) { 
-          return res.render('product-add', { error: 'Not authorized.' });
-      } //si no incio sesion, no mostrar 
-      req.body.id = req.session.user.id_user; //poder identifcar quien esta cargando el producto
-      if (req.file) req.body.cover = (req.file.path).replace('public', '');
-      db.Product.create({
-        name: req.body.name,
-        description: req.body.description,
-        picture_product: req.body.picture_product,
-      }, 
-      // creo que el problema viene de que no incluimos esto pero no donde meterlo o como arreglarlo
-      // {
-      //   include: [
-      //     {association: "owner"},
-      //     {association: "comments"}
-      //   ]
-      // }
-      // si pondemos db.Product.create(req.body) --> da el mismo error 
+  },
+  show: function (req, res) {
+    product.findByPk(req.params.id, {
+        include: {
+          all: true,
+          nested: true
+        }
+      }).then(function (Product) {
+        console.log(Product.dataValues);
+        res.render('product', {
+          products: Product,
+          comments: Comments,
+          user: db.User,
+        })
+      })
+      .catch(function (error) {
+        res.send(error)
+      });
+  },
+
+  // Funcionalidad de ADD 
+  productAdd: function (req, res) {
+    if (!req.session.user) {
+      throw Error('Not authorized')
+    }
+    res.render('product-add')
+  },
+  storeAdd: function (req, res) {
+    if (!req.session.user) {
+      return res.render('product-add', {
+        error: 'Not authorized.'
+      });
+    } //si no incio sesion, no mostrar 
+    req.body.id = req.session.user.id_user; //poder identifcar quien esta cargando el producto
+    if (req.file) req.body.cover = (req.file.path).replace('public', '');
+    db.Product.create({
+          name: req.body.name,
+          description: req.body.description,
+          picture_product: req.body.picture_product,
+        },
+        // creo que el problema viene de que no incluimos esto pero no donde meterlo o como arreglarlo
+        // {
+        //   include: [
+        //     {association: "owner"},
+        //     {association: "comments"}
+        //   ]
+        // }
+        // si pondemos db.Product.create(req.body) --> da el mismo error 
       )
-          .then(function() {
-              res.redirect('/')
-          })
-          .catch(function(error) {
-              res.send(error);
-          })
-  }
+      .then(function () {
+        res.redirect('/')
+      })
+      .catch(function (error) {
+        res.send(error);
+      })
+  },
+  
+  //Funcionalidad edit 
+  edit: function (req, res) { 
+    db.Product.findByPk(req.params.id_product) 
+      .then(function (products) {
+        res.render('product-edit', {
+          products
+        });
+      })
+      .catch(function (error) {
+        res.send(error);
+      })
+  },
+  update: function (req, res) {
+    if (req.file) req.body.picture_product = (req.file.path).replace('public', '');
+    db.Product.update(req.body, {
+        where: {
+          id: req.params.id_product
+        }
+      })
+      .then(function (products) {
+        res.redirect('/')
+      })
+      .catch(function (error) {
+        res.send(error);
+      })
+  },
 }
 
-module.exports= productsController;
+module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,18 @@ var express = require('Express');
 var router = express.Router();
 const productsController = require('../controllers/productsController');
 
+//multer 
+var multer = require('multer');
+const upload = multer({ dest: 'public/images/uploads' });
+
 
 router.get('/', productsController.index);
 router.get('/detail/:id', productsController.show);
 
 router.get('/add', productsController.productAdd); //muestra el formulario de add
-router.post('/add', productsController.storeAdd); //procesa el formulario de add
+router.post('/add', upload.single('picture_product'), productsController.storeAdd); //procesa el formulario de add
+
+router.get('/:id/edit',  productsController.edit);
+router.post('/:id/edit', upload.single('picture_product'),  productsController.update);
 
 module.exports = router;
