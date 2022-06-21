3f1d211910002a62747ffd6ccc62b7e68e4a317a
aldulescano <lescanoa@udesa.edu.ar>
2022-06-20 22:32:21 -0300

    hasheo edit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -126,14 +126,18 @@ const usersController = {
     },
     update: function (req, res) {
         if (req.file) req.body.profile_photo = (req.file.path).replace('public', '');
-        if (req.body.username == db.User.findOne({ where: { username: req.body.username } })) {
-            errors.message = "Email existente."
-            res.locals.errors = errors;
-            return res.render('profile-edit')
-        };
+        // if (req.body.username == db.User.findOne({ where: { username: req.body.username } })) {
+        //     errors.message = "Email existente."
+        //     res.locals.errors = errors;
+        //     return res.render('profile-edit')
+        // };
         // esto no anda, pero deberiamos con lo que nos conteste luis ver si lo arreglamos para que no puedan cambiar su mail por alguno ya registrado
-        // const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        // password: hashedPassword habria que meter esto pero no se como
+
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+        } else{
+            req.body.password = undefined
+        }
         db.User.update(req.body, { where: { id_user: req.params.id } })
             .then(function (users) {
                 res.redirect('/')
